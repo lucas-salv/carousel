9135535387455c0e72f433bd19e9825a3ad607c5
salv <salvadorbr.dev@gmail.com>
2019-09-08 23:58:54 -0300

    basic functions were done

--- js/index.js
+++ js/index.js
@@ -4,26 +4,55 @@
 
     // DOM variables
     let $imgContainer = document.querySelector(".img-container");
-    console.log($imgContainer.children.length);
+    let $btnLeft = document.querySelector(".btn-left");
+    let $btnRight = document.querySelector(".btn-right");
 
     // Others variables
     let cont = 0;
+    let percentTransform = 100;
+    let interval;
     
     // init functions
     imgInterval();
+    btnsChangeImg();
 
     // Functions
     function imgInterval(){
-        console.log(cont);
+        $imgContainer.style.transform = `translateX(-${cont}%)`;
+        cont += percentTransform;
+        if(cont > percentTransform * $imgContainer.children.length){
+            cont = 0;
             $imgContainer.style.transform = `translateX(-${cont}%)`;
-            cont += 100;
-            if(cont > 100 * $imgContainer.children.length){
-                cont = 0;
-                $imgContainer.style.transform = `translateX(-${cont}%)`;
-                cont = 100;
+            cont = percentTransform;
+        }
+        interval = setTimeout(imgInterval, 5000);
+    };
+
+    function btnsChangeImg() {
+        $btnLeft.addEventListener('click', e=>{
+            cont -= percentTransform;
+            e.preventDefault();
+            if(cont > 0){
+                cont -= percentTransform;
+                $imgContainer.style.transform = `translateX(-${cont}%)`; 
+                clearTimeout(interval);
+                imgInterval();
             }
-        setTimeout(imgInterval, 5000);
+            
+        });
+        $btnRight.addEventListener('click', e=>{
+            e.preventDefault();
+            cont -= percentTransform;
+            if(cont < percentTransform * $imgContainer.children.length){
+                cont += percentTransform;
+                $imgContainer.style.transform = `translateX(-${cont}%)`;
+                console.log(cont);
+                clearTimeout(interval);
+                imgInterval();
+            }
+        })
     }
+
 
 
 })();
